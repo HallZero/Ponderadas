b8447d6e4a362c55eb8734a7261f3b6d6315792d
HallZero <filipikikuchi@gmail.com>
2023-08-27 11:56:09 -0300

    feat(routes): :sparkles: Add new routes for testing login and todo

--- ponderada2/app.js
+++ ponderada2/app.js
@@ -1,16 +1,26 @@
 const express =  require('express');
 const {createTask, readTask, updateTask, deleteTask} = require('./task');
-const {createUser, readUser, updateUser, deleteUser} = require('./user');
+const {createUser, readUserByName, updateUser, deleteUser} = require('./user');
+const loginRoute = require('./routes/login');
+const newTask = require('./routes/newTask');
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    // Send the login page
+    res.sendFile(__dirname + '/login.html');
 });
 
+app.get('/todo', (req, res) => {
+    // Send the todo page
+    res.sendFile(__dirname + '/todo.html');
+});
+
+app.post('/login', loginRoute);
+
 app.get('/create', async (req, res) => {
-    const user = await createUser({name: 'Teste'});
+    const user = await createUser({name: 'teste', password: 'teste123'});
     
     const task = {
         description: 'Teste',
@@ -21,6 +31,14 @@ app.get('/create', async (req, res) => {
     const new_task = await createTask(task);
 
     res.send(new_task);
+});
+
+app.post('/task', newTask);
+
+app.get('/get-task', async (req, res) => {
+    const task = await readTask({user_id: 1});
+
+    res.send(task);
 });
 
 app.listen(port, () => {
