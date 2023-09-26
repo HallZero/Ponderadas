a0ed8fa7d162e0d1efb5403590c0f677879e604c
HallZero <filipikikuchi@gmail.com>
2023-09-26 12:57:30 -0300

    feat(dashboard): :sparkles: Add first version of dashboard

--- ponderada2/app.js
+++ ponderada2/app.js
@@ -1,27 +1,37 @@
 const express =  require('express');
-const {createTask, readTask, updateTask, deleteTask} = require('./task');
-const {createUser, readUserByName, updateUser, deleteUser} = require('./user');
+const {createTask, readTask, updateTask, deleteTask} = require('./models/task');
+const {createUser, readUserByName, updateUser, deleteUser} = require('./models/user');
 const bodyParser = require('body-parser');
 const loginRoute = require('./routes/login');
 const newTask = require('./routes/newTask');
+const path = require('path')
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
+    res.sendFile(path.join(__dirname, 'public', '/login.html'));
 });
 
 app.get('/todo', (req, res) => {
-    res.sendFile(__dirname + '/todo.html');
+    res.sendFile(path.join(__dirname, 'public', '/todo.html'));
+});
+
+app.get('/create', async (req, res) => {
+    const user = await createUser({name: 'teste', password: 'teste123'});
 });
 
 app.post('/login', loginRoute);
 
 app.post('/task', newTask);
+
+app.get('/chart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '/charts.html'));
+})
 
 app.get('/get-task', async (req, res) => {
     const task = await readTask({user_id: 1});
