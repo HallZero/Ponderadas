9e385e1569376a8bc961fb9c69a5b9c51011acc8
HallZero <filipikikuchi@gmail.com>
2023-10-02 22:15:04 -0300

    fix(app2): :bug: Rerurn ponderada2 app.js to its original state

--- ponderada4/app.js
+++ ponderada4/app.js
@@ -1,46 +1,27 @@
 const express =  require('express');
-const {createTask, readTask, updateTask, deleteTask} = require('./models/task');
-const {createUser, readUserByName, updateUser, deleteUser} = require('./models/user');
+const {createTask, readTask, updateTask, deleteTask} = require('./task');
+const {createUser, readUserByName, updateUser, deleteUser} = require('./user');
 const bodyParser = require('body-parser');
 const loginRoute = require('./routes/login');
 const newTask = require('./routes/newTask');
-const path = require('path')
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', '/login.html'));
+    res.sendFile(__dirname + '/login.html');
 });
 
 app.get('/todo', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', '/todo.html'));
-});
-
-app.get('/create', async (req, res) => {
-    try{
-        const user = await createUser({name: 'teste', password: 'teste123'});
-        res.json({ message: 'User created successfully!', user });
-    } catch (error) {
-        res.status(500).json({ error: 'User creation failed.' });
-    }
+    res.sendFile(__dirname + '/todo.html');
 });
 
 app.post('/login', loginRoute);
 
 app.post('/task', newTask);
-
-app.get('/chart', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', '/charts.html'));
-})
-
-app.get('/get-data', (req, res) => {
-    res.send({x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]});
-})
 
 app.get('/get-task', async (req, res) => {
     const task = await readTask({user_id: 1});
